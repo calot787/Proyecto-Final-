15c87263e1b0c3c64dc3fb52feb5efed1933166a
Carla Navarro <canagom09@gmail.com>
2026-01-09 16:04:11 +0100

    Carpeta creada con la creación del proyecto

---
+++ Springboot/letrana/src/main/java/com/example/letrana/LetranaApplication.java
@@ -0,0 +1,13 @@
+package com.example.letrana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetranaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetranaApplication.class, args);
+	}
+
+}
---
+++ Springboot/letrana/src/test/java/com/example/letrana/LetranaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.letrana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetranaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
